a1b860b885cdc09163b4546f374abd22a235bdae
haricnugraha <haricnugraha@gmail.com>
2023-01-24 18:29:47 +0700

    fix: remove unnecessary code

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -50,8 +50,7 @@ export function Footer() {
             </Link> */}
           </div>
           <p className="mt-6 text-sm text-slate-500 sm:mt-0">
-            Copyright &copy; {new Date().getFullYear()} TaxPal. All rights
-            reserved.
+            Copyright &copy; {new Date().getFullYear()}. All rights reserved.
           </p>
         </div>
       </Container>
