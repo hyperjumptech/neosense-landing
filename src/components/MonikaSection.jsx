3987ff4ad1b91f84605d1f07d5e4213a3312f0c9
Lukman Adisaputro <lukman.adisaputro@gmail.com>
2023-01-26 16:03:10 +0700

    task-7

---
+++ src/components/MonikaSection.jsx
@@ -0,0 +1,29 @@
+import { Container } from '@/components/Container'
+import { Button } from '@/components/Button'
+
+export function MonikaSection() {
+  return (
+    <section
+      id="secondary-features"
+      aria-label="Features for simplifying everyday business tasks"
+      className="pt-20 pb-14 sm:pb-20 sm:pt-32 lg:pb-32"
+    >
+      <Container>
+        <div className="mx-auto max-w-2xl md:text-center">
+          <h2 className="font-display text-3xl tracking-tight text-slate-900 sm:text-4xl">
+            Open source powered
+          </h2>
+          <p className="mt-4 text-lg tracking-tight text-slate-700">
+            Neosense is powered by the free and open source synthetic monitoring command line tools called Monika by Hyperjump.
+          </p>
+          <Button
+            href="https://monika.hyperjump.tech/"
+            className="mt-10"
+          >
+            Check it out
+          </Button>
+        </div>
+      </Container>
+    </section>
+  )
+}
