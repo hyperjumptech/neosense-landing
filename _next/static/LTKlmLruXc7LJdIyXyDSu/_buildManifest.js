9d8a73b25eb81e1c40afb2a2a9d4bac052d94a38
dennypradipta <denny.aditya.p@gmail.com>
2023-01-30 08:05:50 +0000

    Deploying to gh-pages from @ hyperjumptech/neosense-landing@88b931b7f9359ff6b915d1f3da6d1db8d010cdae 🚀

--- _next/static/LTKlmLruXc7LJdIyXyDSu/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/539-f9c3a56aa7776aa6.js","static/chunks/pages/index-d9a55c2f4b1d0caf.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
