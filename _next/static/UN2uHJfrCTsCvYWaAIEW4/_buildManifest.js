faf54395859232ed8f6dde1ff2e3e725d757189e
haricnugraha <15191978+haricnugraha@users.noreply.github.com>
2023-02-09 03:16:22 +0000

    Deploying to gh-pages from @ hyperjumptech/neosense-landing@f418f090c002bced71595f1f54a2c110aa4b9e6f 🚀

--- _next/static/UN2uHJfrCTsCvYWaAIEW4/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/539-f9c3a56aa7776aa6.js","static/chunks/pages/index-42a10a448a494858.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
